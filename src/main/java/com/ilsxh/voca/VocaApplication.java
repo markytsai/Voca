23039955e15451f9741d32b40b1923f4eedbf8ed
markytsai <cc1290840247@126.com>
2019-01-11 11:20:39 +0800

    新建test.html文件，测试

---
+++ src/main/java/com/ilsxh/voca/VocaApplication.java
@@ -0,0 +1,14 @@
+package com.ilsxh.voca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocaApplication.class, args);
+	}
+
+}
+
